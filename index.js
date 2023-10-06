6e0d77e943861bdc11010f61fef1f180a965cfe8
Raphael Bajet <baphael@gmail.com>
2023-10-05 17:58:05 -0700

    'Express on Vercel' is the only thing showing up on my home page, trying to troubleshoot

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-    res.send('Express on Vercel');
   });
 
   app.get('/art/', (req, res) => {
